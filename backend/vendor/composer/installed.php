f3d5172e27d9488f9d8ab2afa70f79fefdfeeeca
Walt <walt.fe.wga@gmail.com>
2023-04-04 19:05:24 +0300

    add first version of wordpress next builder

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'webdevstudios/wds-headless-theme' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ccc7f5fb7c73c13654c2405810f053fb5f04229e',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../webdevstudios/wds-headless-theme',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
